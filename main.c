2dd3be0dee26213a5634c5b8d5f7d475b04517d3
Kevin Mark Price <KPrice725@aol.com>
2019-01-06 20:37:25 -0500

    Add detab function

--- main.c
+++ main.c
@@ -2,14 +2,20 @@
 
 #define MAXLINE 1000
 #define TABSTOPINTERVAL 8
-//detab program to replace tabs in input with the proper number of blanks to space to the next tab stop
+
+/*
+Exercise 1-20: Write a program detab that replaces tabs in the input with the proper number of blanks to space to the next tab stop.
+Assume a fixed set of tab stops, say every n columns.  Should n be a variable or a symbolic parameter?
+*/
 int getline(char line[], int maxLine);
+int detab(char line[], int index);
 
 int main()
 {
     int len;
     char line[MAXLINE];
 
+    //checks to see if getline returns any text, and if so, prints it to the screen.
     while ((len = getline(line, MAXLINE)) > 0) {
         printf("%s\n", line);
     }
@@ -22,14 +28,7 @@ int getline(char s[], int lim) {
     for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i) {
         if (i < lim - 1) {
             if (c == '\t') {
-                int flag, tempIndex;
-                tempIndex = i;
-                flag = tempIndex / TABSTOPINTERVAL;
-                while (flag == (tempIndex / TABSTOPINTERVAL)) {
-                    s[tempIndex] = ' ';
-                    ++tempIndex;
-                }
-                i = tempIndex - 1;
+                i = detab(s, i);
             } else {
                 s[i] = c;
             }
@@ -41,4 +40,14 @@ int getline(char s[], int lim) {
         s[lim - 1] = '\0';
     }
     return i;
+}
+//detab: replaces a '\t' with the necessary number of ' ' character to fill in the line up to the next tab stop
+int detab(char s[], int index) {
+    int flag;
+    flag = index / TABSTOPINTERVAL;
+    while (flag == (index / TABSTOPINTERVAL)) {
+        s[index] = ' ';
+        ++index;
+    }
+    return index - 1;
 }
