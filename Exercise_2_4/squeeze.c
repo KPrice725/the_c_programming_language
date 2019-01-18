434e1400034791c0c00bf843e121bcfb1b50e002
Kevin Price <kprice725@aol.com>
2019-01-18 10:48:07 -0500

    Initial Commit

---
+++ Exercise_2_4/squeeze.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define MAX_CHARACTERS 0xFF		/* 256-Character ASCII Set */
+enum boolean {FALSE, TRUE};
+
+/*
+Exercise 2-4: Write an alternate version of squeeze(s1, s2) that deletes each character in s1 that matches any character in the string s2.
+*/
+void squeeze(char s1[], const char s2[]);
+
+int main() {
+
+	char s1[] = "My name is Kevin!";
+	char s2[] = "Msvn";
+	squeeze(s1, s2); 			/* Should result in "y ame i Kei!" */
+//	printf("%s\n", s1);
+	return 0;
+}
+
+/*
+squeeze: create a characters array with each value initialized to FALSE,
+then iterate through s2 string, setting the value of the characters array at the s2 char's index value to TRUE.
+Then iterate through the s1 string, checking to see if the value in the characters array is TRUE,
+skipping over it, if so.
+*/
+void squeeze(char s1[], const char s2[]) {
+
+	int characters[MAX_CHARACTERS], i, j;
+	for (i = 0; i < MAX_CHARACTERS; ++i) {
+		characters[i] = FALSE;
+	}
+
+	for (i = 0; s2[i] != '\0'; ++i) {
+		characters[s2[i]] = TRUE;
+	}
+
+	for (i = j = 0; s1[i] != '\0'; ++i) {
+		if (characters[s1[i]] == FALSE) {
+			s1[j++] = s1[i];
+		}
+	}
+	s1[j] = '\0';
+}
