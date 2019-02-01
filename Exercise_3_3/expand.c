7c72879c0fb9cdc4713f60ff623c23976b20658c
Kevin Price <kprice725@aol.com>
2019-02-01 09:56:28 -0500

    Update expand.c

--- Exercise_3_3/expand.c
+++ Exercise_3_3/expand.c
@@ -2,6 +2,12 @@
 #include <ctype.h>
 #define MAX_LENGTH 100
 
+/*
+ Exercise 3-3. Write a function expand (s 1, s2) that expands shorthand notations like a - z in the string s1
+ into the equivalent complete list abc ... xyz in s2. Allow for letters of either case and digits,
+ and be prepared to handle cases like a-b-c and a-z0-9 and -a-z. Arrange that a leading or trailing - is taken literally.
+ */
+
 void expand(char s1[], char s2[]) {
 	int i, j, c1, c2;
 	i = j = c1 = c2 = 0;
