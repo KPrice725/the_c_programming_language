78f83345fce3ea6bb342571714c30f1dc1134ff8
Kevin Price <kprice725@aol.com>
2019-02-10 13:46:57 -0500

    Implement more efficient handling

--- Exercise_3_5/itob.c
+++ Exercise_3_5/itob.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define DECIMAL 10
-#define HEX		16
+#define HEX	16
 #define OCTAL	8
 #define BINARY  1
 
@@ -11,43 +11,16 @@ void itoa_hex(int n, char s[]);
 void itoa_binary(int n, char s[]);
 void reverse(char s[]);
 
-//1111 1111 1111 1111 1111 1111 1111 0000
-int main(void) {
-
-	char result[sizeof(int) * 8 + 1];
-	int input1 = 12312555;
-	int input2  = -12312555;
-	int input3 = -10;
-
-//	itob(input, result, DECIMAL);
-//
-//	printf("%s\n", result);
-
-	itob(input1, result, OCTAL);
-
-	printf("%s\n", result);
-
-	itob(input2, result, OCTAL);
-
-	printf("%s\n", result);
-
-	itob(input3, result, OCTAL);
-
-	printf("%s\n", result);
-
-	return 0;
-}
-
 void itob(int number, char output[], char base) {
 
 	switch (base) {
 
 	case BINARY:
-        itoa_binary(number, output);
+        	itoa_binary(number, output);
 		break;
 
 	case OCTAL:
-        itoa_octal(number, output);
+        	itoa_octal(number, output);
 		break;
 
 	case DECIMAL:
@@ -59,7 +32,6 @@ void itob(int number, char output[], char base) {
 		break;
 
 	default:
-
 		break;
 	}
 }
@@ -79,17 +51,11 @@ void itoa_decimal(int n, char s[]) {
 
 void itoa_octal(int n, char s[]) {
 	int i = 0;
-	int targetValue;
 	int sign = (n < 0) ? -1 : 1;
-    if (sign == 1) {
-        targetValue = 07;
-    } else {
-        targetValue = -4000000000;
-    }
 
-    do {
-        s[i++] = sign * (n % 8) + '0';
-    } while ((n /= 10) != 0);
+    	do {
+        	s[i++] = sign * (n % 8) + '0';
+    	} while ((n /= 8) != 0);
 
 	if (sign == -1) {
 		s[i++] = '-';
@@ -99,113 +65,31 @@ void itoa_octal(int n, char s[]) {
 }
 
 void itoa_hex(int n, char s[]) {
+    	int unsigned un = n;
 	int i = 0;
-	int sign = (n < 0) ? -1 : 1;
-	int targetByte;
-	if (sign == 1) {
-		targetByte = 0xF;
-	} else {
-		targetByte = 0xF0000000;
-	}
 
 	do {
-		int unsigned byte = n & targetByte;
-		if (sign == -1) {
-			byte >>= 28;
-		}
-		switch (byte) {
-		case 0x0:
-			s[i++] = '0';
-			break;
-		case 0x1:
-			s[i++] = '1';
-			break;
-		case 0x2:
-			s[i++] = '2';
-			break;
-		case 0x3:
-			s[i++] = '3';
-			break;
-		case 0x4:
-			s[i++] = '4';
-			break;
-		case 0x5:
-			s[i++] = '5';
-			break;
-		case 0x6:
-			s[i++] = '6';
-			break;
-		case 0x7:
-			s[i++] = '7';
-			break;
-		case 0x8:
-			s[i++] = '8';
-			break;
-		case 0x9:
-			s[i++] = '9';
-			break;
-		case 0xA:
-			s[i++] = 'A';
-			break;
-		case 0xB:
-			s[i++] = 'B';
-			break;
-		case 0xC:
-			s[i++] = 'C';
-			break;
-		case 0xD:
-			s[i++] = 'D';
-			break;
-		case 0xE:
-			s[i++] = 'E';
-			break;
-		case 0xF:
-			s[i++] = 'F';
-			break;
-		default:
-			break;
-		}
-		if (sign == 1) {
-			n >>= 4;
-		} else {
-			n <<= 4;
-		}
-	} while (n != 0);
+        	int unsigned value = un % 16;
+        	s[i++] = (value > 9) ? (value - 10 + 'A') : (value + '0');
+	} while ((un /= 16) != 0);
+
 	s[i] = '\0';
-	if (sign == 1) {
-		reverse(s);
-	}
+	reverse(s);
 
 }
 
 void itoa_binary(int n, char s[]) {
-    int i, bit, targetBit;
-  	int sign = (n < 0) ? -1 : 1;
 
-  	if (sign == 1) {
-        targetBit = 0x1;
-  	} else {
-        targetBit = 0x80000000;
-  	}
+  	int unsigned un = n;
+	int i = 0;
 
-    for (i = bit = 0; i < 32; i++) {
-        if (n & targetBit) {
-            s[i] = '1';
-        } else {
-            s[i] = '0';
-        }
-        if (sign == 1) {
-            n >>= 1;
-        } else {
-            n <<= 1;
-        }
-    }
+	do {
+        	int unsigned value = un % 2;
+        	s[i++] = (value == 1) ? '1' : '0';
+	} while ((un /= 2) != 0);
 
-    s[i] = '\0';
-    if (sign == 1) {
-        reverse(s);
-    }
-
+	s[i] = '\0';
+	reverse(s);
 }
 
 void reverse(char s[]) {
